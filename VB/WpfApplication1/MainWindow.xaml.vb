38a613aae4969fe1093b753959798fe796d1f626
DevExpressExampleBot <fakeEmail>
2022-12-18 19:38:27 +0400

    Source auto update [skip ci]

--- VB/WpfApplication1/MainWindow.xaml.vb
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -1,32 +1,22 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.Charts.Designer
 
 Namespace WpfApplication1
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Dim designer As New ChartDesigner(Me.chart)
-			designer.Show(Me)
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim designer As ChartDesigner = New ChartDesigner(Me.chart)
+            designer.Show(Me)
+        End Sub
+    End Class
 End Namespace
